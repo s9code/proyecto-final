56c60ce5f112bcdf2901a9144ab8db7cf6470cb1
S9Code <javirodv@icloud.com>
2023-10-15 21:37:59 +0200

    Update

--- gestion_comics_mangas/servidor/bd.js
+++ gestion_comics_mangas/servidor/bd.js
@@ -4,11 +4,20 @@ const port = 3000;
 
 const mysql = require('mysql2');
 
+// Middleware para procesar datos JSON en solicitudes POST
+app.use(express.json());
+
+// Datos de conexion a la bse de datos
 const conector = mysql.createConnection( {
   host: 'localhost',
   user: 's9code',
   password: '123456',
   database: 'comic_manga'
+});
+
+// Ruta de ejempo para tu inicio web
+app.get('/', (req, res) => {
+  res.send('Bienvenido a tu aplicacion Web!')
 });
 
 app.get('/json', function (req, res) {
--- gestion_comics_mangas/servidor/index.js
+++ gestion_comics_mangas/servidor/index.js
@@ -1,6 +1,5 @@
 const express = require('express');
-const app = express();
-const port = 3000;
+const router = express.Router();
 
 const bd = require('./bd');
 
