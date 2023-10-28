b45ea9036d7962907d3c61afedbb540834f13a23
S9Code <javirodv@icloud.com>
2023-10-28 12:13:11 +0200

    Update

--- gestion_comics_mangas/cliente/src/pages/registro.js
+++ gestion_comics_mangas/cliente/src/pages/registro.js
@@ -7,22 +7,55 @@ function Registro() {
   const [values, setValues] = useState({
     name: '',
     email: '',
-    password: ''
+    password: '',
   })
+
+  const [errors, setErrors] = useState({})
+
   const handleChange = (e) => {
     setValues({...values, [e.target.name]:[e.target.value]})
   }
   const handleSubmit = (e) => {
     e.preventDefault();
-    axios.post('http://localhost:8081/registro', values)
-    .then(res => console.log('Usuario Registrado'))
-    .catch(err => console.log(err))
+    
+    const validationErrors = {}
+
+    if(values.name === '') {
+      validationErrors.name = 'Nombre de usuario requerido'
+    }
+    if(values.email === '') {
+      validationErrors.email = 'Email requerido'
+    }else if (!/\S+@\S+\.\S+/.test(values.email)) {
+      validationErrors.email = 'El email no es valido'
+    }
+    if(values.password === '') {
+      validationErrors.password = 'Contraseña requerida'
+    }
+
+    setErrors(validationErrors)
+
+    if(Object.keys(validationErrors).length === 0) {
+      console.log('usuario registrado')
+      axios.post('http://localhost:8081/registro', values)
+    }else {
+      console.log('Datos de usuario incorrectos')
+    }
+
   }
 
 return (
   <div>
     <h2>Registrate</h2>
     <form onSubmit={handleSubmit}>
+      <div>
+        <label htmlFor='name'>Nombre de Usuario:</label>
+        <input type='text'
+        placeholder='Introduce el nombre'
+        name='name'
+        onChange={handleChange}>
+        </input>
+        {errors.name && <span>{errors.name}</span>}
+      </div>
       <div>
         <label htmlFor='email'>Email:</label>
         <input 
@@ -31,14 +64,7 @@ return (
         name='email'
         onChange={handleChange}>
         </input>
-      </div>
-      <div>
-        <label htmlFor='name'>Nombre de Usuario:</label>
-        <input type='text'
-        placeholder='Introduce el nombre'
-        name='name'
-        onChange={handleChange}>
-        </input>
+        {errors.email && <span>{errors.email}</span>}
       </div>
       <div>
         <label htmlFor='password'>Contraseña:</label>
@@ -46,7 +72,9 @@ return (
         type='password'
         placeholder='Introduce la Contraseña'
         name='password'
-        onChange={handleChange}></input>
+        onChange={handleChange}>
+        </input>
+        {errors.password && <span>{errors.password}</span>}
       </div>
         <button>Crear usuario</button>
       </form>
